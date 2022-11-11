6f021ae472fd26194eece4cfa3a79dffd264db6e
bzceval <busraaceval@gmail.com>
2022-11-12 01:00:52 +0300

    add get data function

--- src/utils/firebaseFunctions.js
+++ src/utils/firebaseFunctions.js
@@ -1,15 +1,37 @@
 // Data Reading and Writing in Firebase DB
-import firebase from './firebase'
-import { getDatabase, ref, push, set } from "firebase/database";
+import firebase from "./firebase";
+import { getDatabase, ref, push, set, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
 
-export const AddUser=(info)=>{
+//create new user
+export const AddUser = (info) => {
   const db = getDatabase(firebase);
-  const userRef=ref(db,"users/")
-  const newUserRef=push(userRef);
-  set(newUserRef,{
-      username:info.username,
-      phoneNumber:info.phoneNumber,
-      gender:info.gender
-  })
+  const userRef = ref(db, "users/");
+  const newUserRef = push(userRef);
+  set(newUserRef, {
+    username: info.username,
+    phoneNumber: info.phoneNumber,
+    gender: info.gender,
+  });
+};
 
+// get data
+export const useUser=()=>{
+  const [isLoading,setIsLoading]=useState();
+  const [contactList,setContactList]=useState();
+ useEffect(() => {
+     const db = getDatabase(firebase);
+     const userRef=ref(db,"users/")
+     onValue(userRef,(snapshot)=>{
+         const data=snapshot.val();
+         const userArray=[]
+
+         for (let id in data){
+             userArray.push({id,...data[id]})
+         }
+         setContactList(userArray)
+         setIsLoading(false)
+     })
+ },[])
+ return {isLoading,contactList}
 }
