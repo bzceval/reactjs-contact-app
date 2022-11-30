d96b5c5595ab99f9d29430a7c68516b0ec50ffec
bzceval <busraaceval@gmail.com>
2022-11-30 18:41:41 +0300

    change contact list design

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -13,58 +13,28 @@ const ContactForm = ({ info, setInfo, handleSubmit }) => {
     setInfo({ ...info, [name]: value });
   };
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="contact-form w-100 shadow rounded p-4"
-    >
+    <form onSubmit={handleSubmit} className="contact-form w-100 shadow rounded p-4" >
       <div className="mb-3 pe-5 ps-5">
-        <label htmlFor="username" className="form-label mb-1">
-          Fullname
-        </label>
-        <input
-          onChange={handleChange}
-          name="username"
-          value={info.username}
-          type="text"
-          className="form-control"
-          id="username"
-        />
+        <label htmlFor="username" className="form-label mb-1"> Fullname </label>
+        <input onChange={handleChange} name="username" value={info.username} type="text" className="form-control" id="username" />
       </div>
+
       <div className="mb-3 pe-5 ps-5">
-        <label htmlFor="phoneNumber" className="form-label mb-1">
-          Phone Number
-        </label>
-        <input
-          onChange={handleChange}
-          name="phoneNumber"
-          value={info.phoneNumber}
-          type="text"
-          className="form-control"
-          id="phoneNumber"
-        />
+        <label htmlFor="phoneNumber" className="form-label mb-1"> Phone Number </label>
+        <input onChange={handleChange} name="phoneNumber" value={info.phoneNumber} type="text" className="form-control" id="phoneNumber" />
       </div>
+
       <div className="mb-3 pe-5 ps-5">
-        <label htmlFor="gender" className="form-label mb-1">
-          Gender
-        </label>
-        <select
-          onChange={handleChange}
-          name="gender"
-          value={info.gender}
-          id="gender"
-          className="form-select"
-          aria-label="Please select gender"
-        >
+        <label htmlFor="gender" className="form-label mb-1"> Gender </label>
+        <select onChange={handleChange} name="gender" value={info.gender} id="gender" className="form-select" aria-label="Please select gender" >
           <option value="male">Male</option>
           <option value="female">Female</option>
           <option value="other">Other</option>
         </select>
       </div>
+
       <div className="mb-3 d-grid mt-4 pe-5 ps-5">
-        <button type="submit" className="btn btn-primary">
-          {" "}
-          Add Contact{" "}
-        </button>
+        <button type="submit" className="btn btn-primary">Add Contact</button>
       </div>
     </form>
   );
