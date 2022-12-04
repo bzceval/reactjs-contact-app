1a389a6e3a666ccf22d5e3ce91ffe5ce67f203ef
bzceval <busraaceval@gmail.com>
2022-12-05 01:41:19 +0300

    login display screen design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import Bird from "../assets/Bird.gif";
 import { MainContext } from "../context/ContextProvider";
 import { logOut } from "../utils/authFunctions";
+import BirdWelcome from "../assets/BirdWelcome.png";
 
 const Home = () => {
   const navigate = useNavigate()
@@ -10,14 +11,17 @@ const Home = () => {
   return ( 
     <div className="p-3 mt-4">
       <div className="home-description text-center">
-        <h6 className="fs-2 display-1 text-decoration-underline">Bird Contact</h6> 
+        <h6 className="fs-2 display-1 text-decoration-underline mb-4">Bird Contact</h6> 
         <p>Welcome to the Bird Contact Web App. Access all your connections wherever you are in the world. </p>
       </div>   
-      <div className="user">
-      {user && <div className="btn-container text-center my-3">
-          <button className="btn auth-btn mx-2" onClick={() => logOut()}> Logout</button>
+      <div>
+      {user && <div className="text-center my-5">
+        <div className="user"></div>
+          <img src={BirdWelcome} alt="Bird Welcome" width={60}/>
+          <h5>Hi {user.displayName}, how are you today?</h5>
+          <button className="btn auth-btn mx-2 my-5" onClick={() => logOut()}> Logout</button>
         </div>}
-        {!user && <> <div className="btn-container text-center my-3">
+        {!user && <> <div className="text-center my-3">
         <button className="btn auth-btn mx-2" onClick={() => navigate("/login")}> Login</button>
         <button className="btn auth-btn mx-2" onClick={() => navigate("/register")}> Register</button>
       </div> <div className="text-center">
